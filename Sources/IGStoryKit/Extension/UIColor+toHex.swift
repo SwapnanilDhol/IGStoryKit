0c8b2ea3ce220256319db2b7d053998e4a2a7039
Swapnanil Dhol <swapnanildhol@gmail.com>
2022-03-10 21:06:09 +0530

    UIColor+: Fixed hex formatting

--- Sources/IGStoryKit/Extension/UIColor+toHex.swift
+++ Sources/IGStoryKit/Extension/UIColor+toHex.swift
@@ -50,19 +50,6 @@ extension UIColor {
     }
 
     var toHex: String? {
-        return toHex()
-    }
-
-    func toHexString() -> String {
-        var r:CGFloat = 0
-        var g:CGFloat = 0
-        var b:CGFloat = 0
-        var a:CGFloat = 0
-
-        getRed(&r, green: &g, blue: &b, alpha: &a)
-
-        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
-
-        return String(format:"#%06x", rgb)
+        return "#" + (toHex() ?? "D46F04")
     }
 }
